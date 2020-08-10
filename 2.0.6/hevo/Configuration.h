03643a527a66a3ad3557d070f7a1d9588f61cf05
Fabio De Carli <fabio.decarli@ciss.com.br>
2020-08-10 19:37:00 -0300

    change configuration

--- 2.0.6/hevo/Configuration.h
+++ 2.0.6/hevo/Configuration.h
@@ -983,7 +983,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 30, 0, -2.3 }
+#define NOZZLE_TO_PROBE_OFFSET { 27, 0, -2.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
